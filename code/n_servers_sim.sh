5c7e299397a8878ab2d30641512f63377de2b817
eliransc <scherzter@gmail.com>
2024-01-03 10:03:00 +0200

    initial commit

---
+++ code/n_servers_sim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -t 0-23:58
+#SBATCH -A def-dkrass
+#SBATCH --mem 20000
+source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
+python /home/eliransc/projects/def-dkrass/eliransc/non_renewal_queueing_system/code/single_station_n_servers.py
+
