c902042dfd5483c15bc2c3c66e793eeffd1248a9
eliransc <scherzter@gmail.com>
2024-01-09 13:44:05 +0200

    initial commit

--- code/make_pkl_file_training.py
+++ code/make_pkl_file_training.py
@@ -12,13 +12,16 @@ files = os.listdir(path)
 print('start')
 for ind, file in tqdm(enumerate(files)):
 
-    file_ = pkl.load(open(os.path.join(path, file), 'rb'))
-    if ind == 0:
-        inpt = file_[0].reshape(1, file_[0].shape[0])
-        output = file_[1].reshape(1, file_[1].shape[0])
-    else:
-        inpt = np.concatenate((inpt, file_[0].reshape(1, file_[0].shape[0])), axis=0)
-        output = np.concatenate((output, file_[1].reshape(1, file_[1].shape[0])), axis=0)
+    try:
 
+        file_ = pkl.load(open(os.path.join(path, file), 'rb'))
+        if ind == 0:
+            inpt = file_[0].reshape(1, file_[0].shape[0])
+            output = file_[1].reshape(1, file_[1].shape[0])
+        else:
+            inpt = np.concatenate((inpt, file_[0].reshape(1, file_[0].shape[0])), axis=0)
+            output = np.concatenate((output, file_[1].reshape(1, file_[1].shape[0])), axis=0)
+    except:
+        print('bad input')
 
 pkl.dump((inpt, output), open('/scratch/eliransc/pkl_training/depart_0_training.pkl', 'wb'))
