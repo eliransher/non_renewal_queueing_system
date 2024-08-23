f743f61985f363076349a0527bd04b111a7e197f
eliransher <scherzter@gmail.com>
2024-08-23 09:41:38 +0300

    checking on the double check

--- code/main_deter.sh
+++ code/main_deter.sh
@@ -3,4 +3,4 @@
 #SBATCH -A def-dkrass
 #SBATCH --mem 20000
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/non_renewal_queueing_system/code/main_deter.py
+python /home/eliransc/projects/def-dkrass/eliransc/non_renewal_queueing_system/code/sim_determinstic_train.py
