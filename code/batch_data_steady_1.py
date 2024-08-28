8d075fb0c7e29af9c55bfca81af112ccb33f690b
eliransher <scherzter@gmail.com>
2024-08-28 13:00:19 +0300

    checking on the double check

--- code/batch_data_steady_1.py
+++ code/batch_data_steady_1.py
@@ -6,15 +6,13 @@ import pickle as pkl
 
 
 cluster_name = os.listdir('/scratch/eliransc/cluster_name/')[0]
-path = '/scratch/eliransc/non_renewal/steady_1_train_long'
+path = '/scratch/eliransc/non_renewal/steady_1_train_long2'
 
 if not os.path.exists(path):
     os.mkdir(path)
-files = os.listdir(path)
+true_files = os.listdir(path)
 
-true_files = [file for file in files if 'multi' in file]
-# file_name_used = pkl.load( open('/scratch/eliransc/non_renewal/file_used_steady_1.pkl', 'rb'))
-# true_files = [file for file in true_files if file not in file_name_used]
+
 
 batch_size = 128
 
@@ -25,7 +23,7 @@ if not os.path.exists(path_dump_data_depart_0):
 num_batches = int(len(true_files)/batch_size)
 
 
-for batch_num in tqdm(range(94,  num_batches)):
+for batch_num in tqdm(range( num_batches)):
 
     input_depart_0 = np.array([])
     output_depart_0 = np.array([])
@@ -43,7 +41,7 @@ for batch_num in tqdm(range(94,  num_batches)):
             input_depart_0 = inp.reshape(1, inp.shape[0])
             output_depart_0 = out.reshape(1, out.shape[0])
 
-    batch_name = 'train_long_large_scv_steady_1_from_'+cluster_name+'_batch_num_' + str(batch_num+len(os.listdir(path_dump_data_depart_0)))+'.pkl'
+    batch_name = 'train_long2_steady_1_from_'+cluster_name+'_batch_num_' + str(batch_num+len(os.listdir(path_dump_data_depart_0)))+'.pkl'
 
     pkl.dump((input_depart_0, output_depart_0), open(os.path.join(path_dump_data_depart_0, batch_name), 'wb'))
     # pkl.dump(file_name_used, open('/scratch/eliransc/non_renewal/file_used_steady_1.pkl', 'wb'))
