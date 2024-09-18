64917bc1ac95407760f887ccdeb7d9af995b3002
eliransher <scherzter@gmail.com>
2024-09-18 21:13:03 +0300

    checking on the double check

--- code/main.sh
+++ code/main.sh
@@ -3,5 +3,5 @@
 #SBATCH -A def-dkrass
 #SBATCH --mem 20000
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/non_renewal_queueing_system/code/main.py
+python /home/eliransc/projects/def-dkrass/eliransc/non_renewal_queueing_system/code/sim_large_scv_large_util_long_sim.py
 
