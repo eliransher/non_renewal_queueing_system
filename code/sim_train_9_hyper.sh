3d7bef1f06d83f1142fd87f761aff1738a9b1e5f
eliransher <scherzter@gmail.com>
2024-10-14 12:52:52 +0300

    checking on the double check

--- code/sim_train_9_hyper.sh
+++ code/sim_train_9_hyper.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #SBATCH -t 0-23:58
 #SBATCH -A def-dkrass
-#SBATCH --mem 2
+#SBATCH --mem 20000
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
 python /home/eliransc/projects/def-dkrass/eliransc/non_renewal_queueing_system/code/sim_you_hyper_for_training.py
+
+
+
