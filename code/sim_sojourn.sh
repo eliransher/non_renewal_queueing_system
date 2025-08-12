1dcf4ca982d36b090f9873d1344115cd1cb80729
eliransher <scherzter@gmail.com>
2025-08-12 10:02:26 +0300

    checking on the double check

--- code/sim_sojourn.sh
+++ code/sim_sojourn.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=sample_ph            # Job name
 #SBATCH --account=power-general-users
-#SBATCH --time=22:00:00
+#SBATCH --time=32:00:00
 #SBATCH --partition=power-general
 #SBATCH --ntasks=1                      # Number of tasks per array job
 #SBATCH --nodes=1                       # Number of nodes
 #SBATCH --cpus-per-task=1               # CPUs per task
-#SBATCH --mem-per-cpu=4G                # Memory per CPU
+#SBATCH --mem-per-cpu=15G                # Memory per CPU
 #SBATCH --output=sample_ph%A_%a.out    # Output file: Job ID and array task ID
 #SBATCH --error=sample_ph%A_%a.err     # Error file: Job ID and array task ID
 
