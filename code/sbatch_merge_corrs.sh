be398cda4a55264a7307817acddc346f460e5d95
eliransher <scherzter@gmail.com>
2026-02-17 13:15:08 +0200

    checking on the double check

---
+++ code/sbatch_merge_corrs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH -t 0-23:58
+#SBATCH -A def-dkrass
+#SBATCH --mem 20000
+source /home/eliransc/projects/def-dkrass/eliransc/mom_match/bin/activate
+python /home/eliransc/projects/def-dkrass/eliransc/non_renewal_queueing_system/code/training_corrs_narval.py
